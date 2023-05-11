fxzjshm/MWA-vcstools

commit 8fb96b28087371f5399c0cd9bcaed86fc8a82275

Fix compile error when all treated as C++ source
some old compilers treat all HIP source as C++ source,
so `extern "C"` is needed even in C sources.
Affected versions: 2.9, 4.0.1, 4.3
Not affected versions: 4.5.2, 5.5

Signed-off-by: fxzjshm <fxzjshm@163.com>

make_beam/beam_psrfits.h
@@ -10,6 +10,10 @@
 #include "psrfits.h"
 #include "mycomplex.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void printf_psrfits( struct psrfits *pf );  /* Prints values in psrfits struct to stdout */
 
 void populate_psrfits_header(
@@ -34,5 +38,8 @@ void correct_psrfits_stt( struct psrfits *pf );
 void psrfits_write_second( struct psrfits *pf, float *data_buffer, int nchan,
         int outpol, int p);
 
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif

CMakeLists.txt
@@ -36,7 +36,11 @@ find_package(FFTW3 COMPONENTS single openmp)
 find_package(XGPU)
 find_package(HYPERBEAM)
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O2 -fPIC -Wall -Wextra ${OpenMP_C_FLAGS}")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O2 -fPIC -Wall -Wextra")
+
+if(OpenMP_C_FOUND)
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
+endif()
 
 EXECUTE_PROCESS( COMMAND gcc -dumpversion GCC_VERSION )
 if(GCC_VERSION EQUAL 7.30)

make_beam/beam_common.c
@@ -240,7 +240,7 @@ void flatten_bandpass(int nstep, int nchan, int npol, void *data)
 
 
     // accumulate abs(data) over all time samples and save into band
-    data_ptr = data;
+    data_ptr = (float *) data;
     for (i=0;i<nstep;i++) { // time steps
         for (p = 0;p<npol;p++) { // pols
             for (j=0;j<nchan;j++){ // channels
@@ -252,7 +252,7 @@ void flatten_bandpass(int nstep, int nchan, int npol, void *data)
     }
 
     // calculate and apply the normalisation to the data
-    data_ptr = data;
+    data_ptr = (float *) data;
     for (i=0;i<nstep;i++) {
         for (p = 0;p<npol;p++) {
             for (j=0;j<nchan;j++){

make_beam/beam_psrfits.c
@@ -10,10 +10,17 @@
 #include <math.h>
 #include "star/pal.h"
 #include "star/palmac.h"
-#include "psrfits.h"
 #include "beam_common.h"
 #include "beam_psrfits.h"
 #include "mycomplex.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "psrfits.h"
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 void printf_psrfits( struct psrfits *pf) {
     fprintf(stdout, "\nPSRFITS:\n");
@@ -311,7 +318,7 @@ void psrfits_write_second( struct psrfits *pf, float *data_buffer, int nchan,
 //    }
     // pointing_offset makes the buffer start at the memory assigned the pointing
     int pointing_offset = p * sec_size;
-    float *pointing_buffer  = malloc( sec_size * sizeof(float) );
+    float *pointing_buffer  = (float *) malloc( sec_size * sizeof(float) );
     memcpy(pointing_buffer, data_buffer + pointing_offset, sec_size * sizeof(float) );
     float_to_unit8( pointing_buffer, sec_size, out_buffer_8);
     

make_beam/form_beam.h
@@ -29,6 +29,10 @@ typedef hipStream_t hiplike_stream_t;
 #define ANT2INC(ant)    (((ant)>>3)&0x03)          /* inc = (ant/8) % 4      */
 #define AP2REC(ant,pol) ((((ant)<<1)+(pol))&0x0F)  /* rec = (2*ant+pol) % 16 */
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /* structure for managing data arrays to be allocated on both host and device */
 struct gpu_formbeam_arrays
 {
@@ -136,5 +140,9 @@ void populate_weights_johnes( struct gpu_formbeam_arrays *g,
                               ComplexDouble ****complex_weights_array,
                               ComplexDouble *****invJi,
                               int npointing, int nstation, int nchan, int npol );
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif

make_beam/get_delays_small.c
@@ -241,7 +241,7 @@ void utc2mjd(char *utc_str, double *intmjd, double *fracmjd) {
 
     int J=0;
     struct tm *utc;
-    utc = calloc(1,sizeof(struct tm));
+    utc = (struct tm *) calloc(1,sizeof(struct tm));
 
     sscanf(utc_str, "%d-%d-%dT%d:%d:%d",
             &utc->tm_year, &utc->tm_mon, &utc->tm_mday,

make_beam/ipfb.h
@@ -19,6 +19,10 @@ typedef hipStream_t hiplike_stream_t;
 
 #include "mycomplex.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 struct gpu_ipfb_arrays
 {
     int ntaps;
@@ -43,5 +47,9 @@ void malloc_ipfb( struct gpu_ipfb_arrays *g, int ntaps, int nsamples,
         int nchan, int npol, int fil_size, int npointing );
 
 void free_ipfb( struct gpu_ipfb_arrays *g );
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
 
 #endif

commit 6b26be5fae6cb858096992052e0dcfe0eee00530

Fix compile error when all treated as C++ source (2)
Forgot to wrap one header file...

Fixes: 8fb96b2 ("Fix compile error when all treated as C++ source")
Signed-off-by: fxzjshm <fxzjshm@163.com>

make_beam/beam_psrfits.h
@@ -7,12 +7,13 @@
 #ifndef BEAM_PSRFITS_H
 #define BEAM_PSRFITS_H
 
-#include "psrfits.h"
 #include "mycomplex.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#include "psrfits.h"
 
 void printf_psrfits( struct psrfits *pf );  /* Prints values in psrfits struct to stdout */
 